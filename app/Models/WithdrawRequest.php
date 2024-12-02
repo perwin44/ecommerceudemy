ef7faf61acb6e224f697353ad12bf90bf1abbf55
Alaa <pnonjida@gmail.com>
2024-12-02 16:06:15 +0200

    udemyecommerce

---
+++ app/Models/WithdrawRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WithdrawRequest extends Model
+{
+    use HasFactory;
+    function vendor() {
+        return $this->belongsTo(Vendor::class);
+    }
+}
